1883829c1a478de8d434b6107f6bd000f3f1aca4
cherepanovig <cherepanovig@gmail.com>
2024-07-12 11:06:08 +0700

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,13 @@
+# Домашняя работа по уроку "Модули и пакеты"
+
+
+def divide(first, second):
+    if second != 0:
+        res = first / second
+        return res
+    else:
+        return "Ошибка"
+
+
+
+#print(divide(88, 0))
---
+++ true_math.py
@@ -0,0 +1,13 @@
+# Домашняя работа по уроку "Модули и пакеты"
+
+
+#import math
+def divide(first, second):
+    import math
+    if second != 0:
+        return first / second
+    else:
+        return math.inf
+
+
+#print(divide(88, 0))
